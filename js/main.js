ba6cd887be24025a07f9f9a06fcc68ca9e64b05d
AndreyVershinkin <92531488+AndreyVershinkin@users.noreply.github.com>
2022-02-01 13:41:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+alert ('Привет! Это мой первый одностраничный сайт! Закрой это окно и зацени!');
+
+$(function () {
+    $(window).scroll(function() {
+	    $('#header .logo').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateInUpRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#header .wrapper').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("jackInTheBox");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('#learn .skills').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#data .container').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rollIn");
+	        }
+	    });
+	});
+})
